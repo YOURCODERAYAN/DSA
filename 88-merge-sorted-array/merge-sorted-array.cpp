10d5597e129530c58f7b56cd6a13651cdee3b5a1
Ayan Saha <ayanwork2003@gmail.com>
2025-08-08 23:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.61%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,19 +1,41 @@
 class Solution {
+private:
+    void swapIfGreater(vector<int> &nums1,vector<int>& nums2,int index1,int index2){
+        if(nums1[index1]>nums2[index2]){
+            swap(nums1[index1],nums2[index2]);
+        }
+    }
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-         int i=m-1;
-         int j=n-1;
-         int k=m+n-1;
-         while(i>=0&&j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
+        //length of imaginary single array
+        int len=n+m;
+        //initial gap
+        int gap=(len/2)+(len%2);
+
+        while(gap>0){
+            //placing of two pointers
+            int left=0;
+            int right=left+gap;
+        while(right<len){
+            //case1: when leftpointer is on arr1[] and rightpointer is on arr2[]
+            if(left<m && right>=m){
+                swapIfGreater(nums1,nums2,left,right-m);
+            }
+            else if(left>=m){
+                swapIfGreater(nums2,nums2,left-m,right-m);
             }
             else{
-                nums1[k--]=nums2[j--];
+                swapIfGreater(nums1,nums1,left,right);
             }
-         }
-         while(j>=0){
-            nums1[k--]=nums2[j--];
-         }
+            left++,right++;
+        }
+        if(gap==1) break;
+
+         gap=(gap/2)+(gap%2);
     }
+
+    for(int i=0;i<n;i++){
+        nums1[m+i]=nums2[i];
+    }
+}
 };
