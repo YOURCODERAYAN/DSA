7c81ca908ee5f6c678cbd188c4b53657c976e311
Ayan Saha <ayanwork2003@gmail.com>
2025-10-15 13:38:42 +0530

    Time: 3 ms (65.46%) | Memory: 10.3 MB (54.02%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
      bool isSafe1(int row , int col , vector<string>&board , int n){
 
@@ -55,5 +55,43 @@ public:
         }
         solve(0 , board , ans , n);
         return ans;
+    }
+};*/
+
+class Solution{
+    public:
+        void solve(int col , vector<string>&board , vector<vector<string>>&ans , vector<int>&leftRow ,       vector<int>& upperD , vector<int>&lowerD ,int n){
+            if(col==n){
+                ans.push_back(board);
+                return;
+            }
+
+        for(int row=0 ; row <n ; row++){
+            if(leftRow[row]==0 && lowerD[row+col]==0 &&upperD[n-1+col-row]==0){
+                board[row][col]='Q';
+            
+        leftRow[row]=1;
+        lowerD[row+col]=1;
+        upperD[n-1+col-row]=1;
+        solve(col+1 , board , ans , leftRow , upperD , lowerD , n);
+            board[row][col]='.';
+            leftRow[row]=0;
+        lowerD[row+col]=0;
+        upperD[n-1+col-row]=0;
+        }
+    }
+ }
+    public:
+        vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        
+        vector<int> leftRow(n,0) , upperD(2*n-1 , 0) , lowerD(2*n-1,0);
+        solve(0 , board , ans , leftRow , upperD , lowerD , n);
+            return ans;
     }
 };
