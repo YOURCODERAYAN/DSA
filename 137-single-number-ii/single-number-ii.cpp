0e44baa973c8c7a824f06c2c63bd70dacc5de14b
Ayan Saha <ayanwork2003@gmail.com>
2025-10-29 14:34:44 +0530

    Time: 7 ms (5.16%) | Memory: 14.1 MB (11.24%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+/*public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
     int ones=0 , twos=0;
@@ -8,5 +8,23 @@ public:
         twos=(twos^nums[i]) & ~ ones;
     }
     return ones;
+    }
+*/
+public:
+    int singleNumber(vector<int>& nums){
+        int n=nums.size();
+
+        map<int , int > mpp;
+        for(int i=0;i<n;i++){
+
+            mpp[nums[i]]++;
+        }
+
+
+        for(auto it: mpp){
+            if(it.second==1)
+                return it.first;
+        }
+        return 0;
     }
 };
